4d46074d6064a810560cb4cfd3057433eb921a2c
sachinnm2 <sachinnmohan@gmail.com>
2025-12-08 23:46:25 +0000

    Define variables for Azure infrastructure
    Added variables for Azure resources configuration.

---
+++ infra/variable.tf
@@ -0,0 +1,29 @@
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "uksouth"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "devops-demo-rg"
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry name (must be globally unique, 5-50 alphanumeric)"
+  type        = string
+  default     = "devopsdemoacr1234"
+}
+
+variable "app_service_plan_name" {
+  description = "App Service Plan name"
+  type        = string
+  default     = "devops-demo-asp"
+}
+
+variable "web_app_name" {
+  description = "Web App for Containers name"
+  type        = string
+  default     = "devops-demo-container-webapp"
+}
