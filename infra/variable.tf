db663e3dc4527be30ef025246ab925feac29be96
sachinnm2 <sachinnmohan@gmail.com>
2025-12-11 13:57:14 +0000

    Refactor variable definitions in variable.tf

--- infra/variable.tf
+++ infra/variable.tf
@@ -1,41 +1,27 @@
 variable "location" {
-  description = "Azure region for all resources"
-  type        = string
-  default     = "uksouth"
+  default = "uksouth"
 }
 
 variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "devops-demo-rg"
+  default = "devops-demo-rg"
 }
 
 variable "acr_name" {
-  description = "Azure Container Registry name (must be globally unique)"
-  type        = string
-  default     = "devopsdemoacr1234"
+  default = "devopsdemoacr9999" # MUST BE UNIQUE
 }
 
-variable "app_service_plan_name" {
-  description = "App Service Plan name"
-  type        = string
-  default     = "devops-demo-asp"
+variable "app_service_plan" {
+  default = "devops-demo-asp"
 }
 
-variable "web_app_name" {
-  description = "Web App for Containers name"
-  type        = string
-  default     = "devops-demo-container-webapp"
+variable "webapp_name" {
+  default = "devops-demo-webapp9999" # MUST BE UNIQUE
 }
 
 variable "image_name" {
-  description = "Container image name (repository) in ACR"
-  type        = string
-  default     = "demo-app"
+  default = "demo-app"
 }
 
 variable "image_tag" {
-  description = "Container image tag"
-  type        = string
-  default     = "latest"
+  default = "latest"
 }
