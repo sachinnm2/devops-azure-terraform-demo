03db84c1e3ae6342d5ae8cc5dbc4f5a6469a1c79
sachinnm2 <sachinnmohan@gmail.com>
2025-12-11 13:57:44 +0000

    Refactor Azure resource definitions in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,14 @@
-##############################################
+# ---------------------------
 # Resource Group
-##############################################
-
+# ---------------------------
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-##############################################
+# ---------------------------
 # Azure Container Registry
-##############################################
-
+# ---------------------------
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
@@ -19,44 +17,38 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-##############################################
+# ---------------------------
 # App Service Plan (Linux)
-##############################################
-
+# ---------------------------
 resource "azurerm_service_plan" "asp" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
+  name                = var.app_service_plan
   resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
 
   os_type  = "Linux"
   sku_name = "B1"
 }
 
-##############################################
-# Web App for Containers (Linux)
-##############################################
-
+# ---------------------------
+# Web App for Containers
+# ---------------------------
 resource "azurerm_linux_web_app" "webapp" {
-  name                = var.web_app_name
+  name                = var.webapp_name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
     application_stack {
-      # This tells App Service which image to pull
       docker_image_name = "${azurerm_container_registry.acr.login_server}/${var.image_name}:${var.image_tag}"
     }
   }
 
   app_settings = {
-    # Port your container listens on
-    WEBSITES_PORT = "3000"
-
-    # ACR registry credentials (simple demo approach)
-    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
+    WEBSITES_PORT                     = "3000"
+    DOCKER_REGISTRY_SERVER_URL        = "https://${azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME   = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD   = azurerm_container_registry.acr.admin_password
   }
 
   https_only = true
