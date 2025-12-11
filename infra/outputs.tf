9fa55dfe9952d04746d92498550df040ca49ebfc
sachinnm2 <sachinnmohan@gmail.com>
2025-12-11 13:58:12 +0000

    Remove descriptions from output variables

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,19 +1,15 @@
 output "acr_login_server" {
-  description = "ACR login server"
-  value       = azurerm_container_registry.acr.login_server
+  value = azurerm_container_registry.acr.login_server
 }
 
 output "acr_name" {
-  description = "ACR name"
-  value       = azurerm_container_registry.acr.name
+  value = azurerm_container_registry.acr.name
 }
 
 output "webapp_name" {
-  description = "Web App name"
-  value       = azurerm_linux_web_app.webapp.name
+  value = azurerm_linux_web_app.webapp.name
 }
 
 output "resource_group_name" {
-  description = "Resource Group name"
-  value       = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name
 }
