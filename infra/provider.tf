9564a763bd518e69c51cc75fecd7e4e7f6b932b3
sachinnm2 <sachinnmohan@gmail.com>
2025-12-12 09:32:00 +0000

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.14.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "~> 4.55.0"
     }
   }
 
