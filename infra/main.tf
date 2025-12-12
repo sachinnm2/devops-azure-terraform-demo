93b7887b03706c1c7c3591390973002d68fe7375
sachinnm2 <sachinnmohan@gmail.com>
2025-12-12 15:11:41 +0000

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -79,12 +79,11 @@ resource "azurerm_linux_web_app" "webapp" {
 # ACR Pull Role Assignment
 # ---------------------------
 resource "azurerm_role_assignment" "acr_pull" {
-  principal_id         = azurerm_linux_web_app.webapp.identity.principal_id
+  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
 
   depends_on = [
-    azurerm_linux_web_app.webapp,
-    azurerm_container_registry.acr
+    azurerm_linux_web_app.webapp
   ]
 }
