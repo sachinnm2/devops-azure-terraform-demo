1f5183aa543f0918775ea637decbcb1c936b7fbc
sachinnm2 <sachinnmohan@gmail.com>
2025-12-12 15:15:08 +0000

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -59,8 +59,6 @@ resource "azurerm_linux_web_app" "webapp" {
     application_stack {
       docker_image_name     = "${var.image_name}:${var.image_tag}"
       docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
-	  docker_registry_username = "${azurerm_container_registry.acr.admin_username}"
-	  docker_registry_password = "${azurerm_container_registry.acr.admin_password}"
     }
   }
 
