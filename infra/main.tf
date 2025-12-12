042bd74fe68c4365f2a76f3a9af07749bf150b1c
sachinnm2 <sachinnmohan@gmail.com>
2025-12-12 14:48:35 +0000

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -50,6 +50,10 @@ resource "azurerm_linux_web_app" "webapp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   service_plan_id     = azurerm_service_plan.asp.id
+  
+  identity {
+    type = "SystemAssigned"
+  }
 
   site_config {
     application_stack {
@@ -68,6 +72,19 @@ resource "azurerm_linux_web_app" "webapp" {
 
   depends_on = [
     azurerm_service_plan.asp,
+    azurerm_container_registry.acr
+  ]
+}
+# ---------------------------
+# ACR Pull Role Assignment
+# ---------------------------
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+
+  depends_on = [
+    azurerm_linux_web_app.webapp,
     azurerm_container_registry.acr
   ]
 }
